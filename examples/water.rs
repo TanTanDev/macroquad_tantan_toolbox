83deea9e76303ec9c8a846e231ef6ce7a37ed863
TanTanDev <mrjonatanOlsson@gmail.com>
2021-03-29 21:35:05 +0200

    fix water rendering

--- examples/water.rs
+++ examples/water.rs
@@ -1,10 +1,9 @@
 use macroquad::prelude::*;
 use macroquad_tantan_toolbox::water::*;
-use macroquad_tantan_toolbox::water;
 
 const GAME_SIZE: Vec2 = Vec2 {
-    x: 512f32,
-    y: 512f32,
+    x: 1344f32,
+    y: 768f32,
 };
 
 #[macroquad::main("water")]
@@ -12,11 +11,10 @@ async fn main() {
     let render_target_game = render_target(GAME_SIZE.x as u32, GAME_SIZE.y as u32);
     set_texture_filter(render_target_game.texture, FilterMode::Nearest);
 
-    let mut camera2d = Camera2D {
-        //zoom: vec2(0.01, 0.01),
+    let camera2d = Camera2D {
         zoom: vec2(1. / GAME_SIZE.x * 2., 1. / GAME_SIZE.y * 2.),
         target: vec2(
-            (GAME_SIZE.x * 0.5f32).floor(),
+            (GAME_SIZE.x * 0.5f32).floor() + (get_time().sin() as f32 * 3.0f32),
             (GAME_SIZE.y * 0.5f32).floor(),
         ),
         render_target: Some(render_target_game),
@@ -27,42 +25,58 @@ async fn main() {
     let tex_water_normal = {
         // All of this is so we can sample the texture repeatedly
         use miniquad;
-        use miniquad::{TextureFormat, FilterMode, TextureParams, TextureWrap};
-        let ctx = unsafe{get_internal_gl().quad_context};
-        let texture_miniquad = miniquad::graphics::Texture::from_data_and_format(ctx, &tex_water_raw.bytes, TextureParams{
-            format: TextureFormat::RGBA8,
-            wrap: TextureWrap::Repeat,
-            filter: FilterMode::Linear,
-            width: tex_water_raw.width as u32,
-            height: tex_water_raw.height as u32,
-        });
+        use miniquad::{FilterMode, TextureFormat, TextureParams, TextureWrap};
+        let ctx = unsafe { get_internal_gl().quad_context };
+        let texture_miniquad = miniquad::graphics::Texture::from_data_and_format(
+            ctx,
+            &tex_water_raw.bytes,
+            TextureParams {
+                format: TextureFormat::RGBA8,
+                wrap: TextureWrap::Repeat,
+                filter: FilterMode::Linear,
+                width: tex_water_raw.width as u32,
+                height: tex_water_raw.height as u32,
+            },
+        );
         Texture2D::from_miniquad_texture(texture_miniquad)
     };
 
-    let water_size = vec2(GAME_SIZE.x*0.5f32, GAME_SIZE.y*0.5f32);
-    let water_pos = vec2(water_size.x*0.5f32, GAME_SIZE.y*0.5f32);
+    let water_size = vec2(GAME_SIZE.x, GAME_SIZE.y * 0.5f32 + 10f32);
+    let water_pos = vec2(water_size.x * 0.0f32, GAME_SIZE.y * 0.5f32 + 100f32);
     let water_dir = vec2(1.0f32, 0.0f32);
     let water_speed = 0.05f32;
-    let water_strength = 0.05f32;
-    let mut water = Water::new(water_pos, water_size, tex_water_normal, water_dir, water_speed, water_strength);
+    let water_strength = 0.02f32;
+    let mut water = Water::new(
+        water_pos,
+        water_size,
+        render_target_game.texture,
+        tex_water_normal,
+        water_dir,
+        water_speed,
+        water_strength,
+        0f32,
+    );
+
+    let beautiful = load_texture("examples/resources/dig_escape_snap.png").await;
+    set_texture_filter(beautiful, FilterMode::Nearest);
     loop {
         // draw first view to render texture
         set_camera(camera2d);
         clear_background(BLUE);
+        draw_texture_ex(
+            beautiful,
+            0f32,
+            -200f32,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(GAME_SIZE.x, GAME_SIZE.y)),
+                ..Default::default()
+            },
+        );
 
-        // draw random stuff
-        for i in 0..700 {
-            rand::srand(i);
-            draw_rectangle(rand::gen_range(0f32, GAME_SIZE.x), rand::gen_range(0f32, GAME_SIZE.y), 10f32, 10f32, Color::new(i as f32/2000f32, 1.0f32, 1.0f32, 1.0f32));
-        }
-
-        let ground_height = 100f32; 
-        //draw_rectangle(GAME_SIZE.x*0.5f32, GAME_SIZE.y*0.5f32+ground_height*0.5f32, GAME_SIZE.x, ground_height, GREEN);
-        draw_rectangle(0f32, GAME_SIZE.y*0.5f32-ground_height, GAME_SIZE.x, ground_height, GREEN);
         water.update(get_frame_time());
-        water.draw_ex(render_target_game.texture, water::DrawParam {flip_y: true});
+        water.draw(&camera2d);
 
-        // draw the transition
         set_default_camera();
         // we wont see yellow because transition is drawn over, but we need to clear anyway
         clear_background(WHITE);
@@ -77,7 +91,23 @@ async fn main() {
                 ..Default::default()
             },
         );
+        draw_ui(&mut water);
 
         next_frame().await
     }
+}
+
+use macroquad::ui::{
+    hash, root_ui,
+    widgets::{self},
+};
+
+fn draw_ui(water: &mut Water) {
+    widgets::Window::new(hash!(), vec2(400., 200.), vec2(320., 400.))
+        .label("Settings")
+        .ui(&mut *root_ui(), |ui| {
+            ui.label(Vec2::new(10., 10.), "HAAH");
+            ui.slider(hash!(), "strength", 0f32..0.4f32, &mut water.strength);
+            ui.slider(hash!(), "speed", 0f32..1f32, &mut water.speed);
+        });
 }
--- src/water.rs
+++ src/water.rs
@@ -1,112 +1,205 @@
-//use macroquad::math::{Vec2};
 use macroquad::prelude::*;
-
-pub struct DrawParam {
-    pub flip_y: bool,
-}
+use miniquad::*;
 
 pub struct Water {
     pos: Vec2,
     size: Vec2,
+    pub rotation: f32,
 
     pub direction: Vec2,
     pub speed: f32,
     pub strength: f32,
 
     offset: Vec2,
-    material: Material,
+    stage: Stage,
+}
+
+#[repr(C)]
+struct Vertex {
+    pos: Vec2,
+    uv: Vec2,
+    sample_uv: Vec2,
+}
+
+struct Stage {
+    pipeline: Pipeline,
+    bindings: Bindings,
+}
+
+#[repr(C)]
+pub struct Uniforms {
+    strength: f32,
+    offset: Vec2,
+    pub view: glam::Mat4,
+    pub model: glam::Mat4,
+}
+
+impl Stage {
+    pub fn new(
+        ctx: &mut Context,
+        tex_water_normal: Texture2D,
+        tex_target: Texture2D,
+        flip_y: bool,
+    ) -> Stage {
+        #[rustfmt::skip]
+        let vertices = match flip_y {
+            true => {
+                [
+                    Vertex { pos : Vec2 { x: -1.0, y: -1.0 }, uv: Vec2 { x: 0., y: 0. }, sample_uv: Vec2 {x: -1., y: -1.} },
+                    Vertex { pos : Vec2 { x:  1.0, y: -1.0 }, uv: Vec2 { x: 1., y: 0. }, sample_uv: Vec2 {x: 1., y: -1.} },
+                    Vertex { pos : Vec2 { x:  1.0, y:  1.0 }, uv: Vec2 { x: 1., y: 1. }, sample_uv: Vec2 {x: 1., y: -3.} },
+                    Vertex { pos : Vec2 { x: -1.0, y:  1.0 }, uv: Vec2 { x: 0., y: 1. }, sample_uv: Vec2 {x: -1., y: -3.} },
+                ]
+            },
+            false => {
+                [
+                    Vertex { pos : Vec2 { x: -1.0, y: -1.0 }, uv: Vec2 { x: 0., y: 0. }, sample_uv: Vec2 {x: -1., y: -3.} },
+                    Vertex { pos : Vec2 { x:  1.0, y: -1.0 }, uv: Vec2 { x: 1., y: 0. }, sample_uv: Vec2 {x: 1., y: -3.} },
+                    Vertex { pos : Vec2 { x:  1.0, y:  1.0 }, uv: Vec2 { x: 1., y: 1. }, sample_uv: Vec2 {x: 1., y: -1.} },
+                    Vertex { pos : Vec2 { x: -1.0, y:  1.0 }, uv: Vec2 { x: 0., y: 1. }, sample_uv: Vec2 {x: -1., y: -1.} },
+                ]
+            }
+        };
+
+        let vertex_buffer = Buffer::immutable(ctx, BufferType::VertexBuffer, &vertices);
+
+        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
+        let index_buffer = Buffer::immutable(ctx, BufferType::IndexBuffer, &indices);
+
+        let bindings = Bindings {
+            vertex_buffers: vec![vertex_buffer],
+            index_buffer,
+            images: vec![
+                tex_target.raw_miniquad_texture_handle(),
+                tex_water_normal.raw_miniquad_texture_handle(),
+            ],
+        };
+
+        let shader_meta = ShaderMeta {
+            images: vec!["tex_water_normal".to_string(), "texture".to_string()],
+            uniforms: UniformBlockLayout {
+                uniforms: vec![
+                    UniformDesc::new("strength", UniformType::Float1),
+                    UniformDesc::new("offset", UniformType::Float2),
+                    UniformDesc::new("view", UniformType::Mat4),
+                    UniformDesc::new("model", UniformType::Mat4),
+                ],
+            },
+        };
+
+        let shader =
+            Shader::new(ctx, WATER_VERTEX_SHADER, WATER_FRAGMENT_SHADER, shader_meta).unwrap();
+
+        let pipeline = Pipeline::new(
+            ctx,
+            &[BufferLayout::default()],
+            &[
+                VertexAttribute::new("pos", VertexFormat::Float2),
+                VertexAttribute::new("uv", VertexFormat::Float2),
+                VertexAttribute::new("sample_uv", VertexFormat::Float2),
+            ],
+            shader,
+        );
+
+        Stage { pipeline, bindings }
+    }
 }
 
 impl Water {
-    pub fn new(pos: Vec2, size: Vec2, tex_water_normal: Texture2D, direction: Vec2, speed: f32, strength: f32) -> Self {
-        let fragment_shader = WATER_FRAGMENT_SHADER.to_string();
-        let vertex_shader = WATER_VERTEX_SHADER.to_string();
-
-        let pipeline_params = PipelineParams {
-            depth_write: true,
-            depth_test: Comparison::LessOrEqual,
-            ..Default::default()
-        };
-
-        let material = load_material(
-            &vertex_shader,
-            &fragment_shader,
-            MaterialParams {
-                textures: vec!["tex_water_normal".to_string()],
-                uniforms: vec![
-                    ("strength".to_string(), UniformType::Float1),
-                    ("offset".to_string(), UniformType::Float2),
-                ],
-                pipeline_params,
-                ..Default::default()
-            },
-        )
-        .unwrap();
-
-        material.set_texture("tex_water_normal", tex_water_normal);
-        material.set_uniform("strength", strength);
+    pub fn new(
+        pos: Vec2,
+        size: Vec2,
+        tex_water_normal: Texture2D,
+        tex_target: Texture2D,
+        direction: Vec2,
+        speed: f32,
+        strength: f32,
+        rotation: f32,
+    ) -> Self {
+        let ctx = unsafe { get_internal_gl().quad_context };
+        let stage = Stage::new(ctx, tex_water_normal, tex_target, true);
         Water {
             pos,
             size,
-            material,
             speed,
+            rotation,
             strength,
             direction: direction.normalize(),
             offset: vec2(0., 0.),
+            stage,
         }
     }
 
     pub fn update(&mut self, dt: f32) {
         self.offset += self.direction * dt * self.speed;
-        self.material.set_uniform("offset", self.offset);
     }
 
-    pub fn draw_ex(&self, base_texture: Texture2D, draw_param: DrawParam) {
-        gl_use_material(self.material);
-        //draw_rectangle(self.pos.x, self.pos.y, self.size.x, self.size.y, YELLOW);
+    pub fn draw(&self, current_camera: &dyn Camera) {
+        let mut gl = unsafe { get_internal_gl() };
 
-        let aspect_err = vec2(base_texture.width()/self.size.x, base_texture.height()/self.size.y);
-        let pos_fix = vec2(self.pos.x / (1.0/aspect_err.x), self.pos.y * aspect_err.y); 
+        // Ensure that macroquad's shapes are not going to be lost
+        gl.flush();
+        gl.quad_context.apply_pipeline(&self.stage.pipeline);
+        let current_pass = current_camera.render_pass();
+        gl.quad_context
+            .begin_pass(current_pass, miniquad::PassAction::Nothing);
+        gl.quad_context.apply_bindings(&self.stage.bindings);
 
-        draw_texture_ex(
-            base_texture,
-            0f32,//pos_fix.x,//self.pos.x,
-            0f32,//pos_fix.y,//self.pos.y,
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(aspect_err),
-                flip_y: draw_param.flip_y,
-                ..Default::default()
-            },
+        let view = current_camera.matrix();
+
+        let half_size = self.size * 0.5f32;
+        let model = glam::f32::Mat4::from_scale_rotation_translation(
+            vec3(half_size.x, half_size.y, 1.0f32),
+            glam::Quat::from_rotation_z(self.rotation),
+            vec3(self.pos.x + half_size.x, self.pos.y + half_size.y, 0f32),
         );
-        gl_use_default_material();
+
+        gl.quad_context.apply_uniforms(&Uniforms {
+            strength: self.strength,
+            offset: self.offset,
+            view,
+            model,
+        });
+        gl.quad_context.draw(0, 6, 1);
+
+        gl.quad_context.end_render_pass();
     }
 }
 
 const WATER_FRAGMENT_SHADER: &'static str = "#version 140
-    in vec2 uv;
+    in vec2 v_uv;
     uniform float strength;
-    // base texture
-    uniform sampler2D Texture;
+    uniform sampler2D texture;
     uniform sampler2D tex_water_normal;
     uniform vec2 offset;
 
     out vec4 color;
 
     void main() {
-        vec4 water_color = texture2D(tex_water_normal, uv+offset);
-        vec4 base_color_offset = texture2D(Texture, uv+(water_color.rg*strength));
+        vec4 water_color = texture2D(tex_water_normal, v_uv+offset);
+        vec4 base_color_offset = texture2D(texture, v_uv+(water_color.rg*strength));
         color = base_color_offset;
     }
 ";
 
 const WATER_VERTEX_SHADER: &'static str = "#version 140
-    in vec3 position;
-    in vec2 texcoord;
-    out vec2 uv;
+    in vec2 pos;
+    in vec2 uv;
+    in vec2 sample_uv;
+    out vec2 v_uv;
+
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform vec2 sample_offset;
+
+    vec2 screen_to_uv(vec2 screen) {
+        return screen * 0.5 + vec2(0.5, 0.5);
+    }
 
     void main() {
-        gl_Position = vec4(position, 1);
-        uv = texcoord;
+        mat4 modelview = view * model;
+        gl_Position = modelview * vec4(pos, 0.0, 1);
+        vec4 uv_sample_offset = modelview * vec4(sample_uv.xy, 0, 1);
+        v_uv = screen_to_uv(uv_sample_offset.xy);
     }
 ";
